2ed1642dce794a98f10e120a72403b0616056858
Sandro Brändli <sandro.braendli@gmail.com>
2019-04-28 12:51:38 +0200

    Hello World

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.21"
+    application
 }
 
 group = "io.braendli"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClassName = "io.braendli.whatsapp_exif_fix.WhatsAppExifFixKt"
+}
 
 repositories {
     mavenCentral()
---
+++ src/main/kotlin/WhatsAppExifFix.kt
@@ -0,0 +1,5 @@
+package io.braendli.whatsapp_exif_fix
+
+fun main() {
+    println("Hello World!")
+}
