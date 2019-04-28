3398c6f6d601f0b868128e70d5f5bc6467cce79c
Sandro Brändli <sandro.braendli@gmail.com>
2019-04-28 16:53:29 +0200

    Set timestamps from file name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,16 @@
 package io.braendli.creation_date_from_name
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributeView
+import java.nio.file.attribute.FileTime
+import java.text.SimpleDateFormat
 
 fun main(args: Array<String>) {
     val root = File(if (args.isEmpty()) ".." else args[0])
     val target = File("./processed/")
     val imagePrefix = "WhatsApp Image "
+    val format = SimpleDateFormat("yyyy-MM-dd HH.mm.ss")
 
     root.walk().filter {
         it.name.startsWith(imagePrefix)
@@ -13,7 +18,13 @@ fun main(args: Array<String>) {
         it.copyTo(File(target, it.name))
     }
 
-    target.walk().forEach {
+    target.walk().filter {
+        !it.name.startsWith(target.name)
+    }.forEach {
+        val dateString = it.name.substring(15, 37).replace("at ", "")
+        val attributes = Files.getFileAttributeView(it.toPath(), BasicFileAttributeView::class.java)
 
+        val time = FileTime.fromMillis(format.parse(dateString).time)
+        attributes.setTimes(time, time, time)
     }
 }
